3ace6e03fed2c075743b357ec5ee260d9c79e01f
xzk <xiaozhikang0916@gmail.com>
2015-06-12 21:36:51 +0800

    playable edition

--- javascript/main.js
+++ javascript/main.js
@@ -17,7 +17,13 @@ var redDrt = direction.Up;
 var tempRedDrt = direction.Up;
 var blueDrt = direction.Down;
 var tempBlueDrt = direction.Down;
-
+var blueEat;
+var redEat;
+var reset;
+var blueWin;
+var redWin;
+var intervarl;
+var speed;
 onload = function () {
 	//The size of canvas should be modified in canvas.height, not canvas.style.height
 	c.height = height * cellSize;
@@ -25,29 +31,44 @@ onload = function () {
 	c.style.border = '1px solid #c3c3c3';
 	redSnack = new Array;
 	blueSnack = new Array;
+
+	document.getElementById('startBtn').addEventListener('click', init);
+	
+
+};
+
+function init() {	
+	var select = document.getElementById('speed').selectedIndex;
+	switch (select){
+		case 0:speed = 200;break;
+		case 1:speed = 500;break;
+		case 2:speed = 800;break;
+	}
+	if (intervarl != undefined){
+		window.clearInterval(intervarl);
+	}
 	resetPosition(5,5);
-	var intervarl = window.setInterval(gameStep, 500);
-
+	intervarl = window.setInterval(gameStep, speed);
 }
-
 //A step in game
 function gameStep() {
-	draw();
+
 	move();
+	draw();
 }
 
-function resetPosition(length1, length2) {
+function resetPosition(red, blue) {
 	var redLength;
 	var blueLength;
-	if (length1 == undefined || length2 == undefined){		
+	if (red == undefined || blue == undefined){		
 	 	redLength = redSnack.length;
 		blueLength = blueSnack.length;
 	} else {
-		redLength = length1;
-		blueLength = length2;
+		redLength = red;
+		blueLength = blue;
 	}
-	redDrt = direction.Up;
-	blueDrt = direction.Down;
+	tempRedDrt = redDrt = direction.Up;
+	tempBlueDrt = blueDrt = direction.Down;
 	redSnack = new Array;
 	blueSnack = new Array;
 	var x;
@@ -69,6 +90,7 @@ function resetPosition(length1, length2) {
 			'y' : y});
 			y -= 1;
 	}
+	draw();
 }
 
 //function to draw frame of the game
@@ -105,7 +127,32 @@ function move() {
 	if (Math.abs(tempRedDrt - redDrt) != 2){
 		redDrt = tempRedDrt;
 	}
-	//Move red snack
+	
+	checkColl();
+	checkWin();
+	if (blueWin){
+		window.alert("Blue win!");
+		window.location.reload();
+		blueWin = false;
+	}
+	if (redWin){
+		window.alert("Red win!");
+		window.location.reload();
+		redWin = false;
+	}
+	if (blueEat){
+		resetPosition(redSnack.length - 1, blueSnack.length + 1);
+		blueEat = false;
+	}
+	if (redEat){
+		resetPosition(redSnack.length + 1, blueSnack.length - 1);
+		redEat = false;
+	}
+	if (reset){
+		resetPosition();
+		reset = false;
+	}
+		//Move red snack
 	var redHead =redSnack[0].clone();
 	switch(redDrt){
 		case direction.Up: redHead.y -= 1;break;
@@ -113,8 +160,6 @@ function move() {
 		case direction.Left: redHead.x -= 1;break;
 		case direction.Right: redHead.x += 1;break;
 	}
-	redSnack.pop();
-	redSnack.unshift(redHead);
 	
 	//Move blue snack;
 	var blueHead =blueSnack[0].clone();
@@ -124,10 +169,90 @@ function move() {
 		case direction.Left: blueHead.x -= 1;break;
 		case direction.Right: blueHead.x += 1;break;
 	}
+	
+	redSnack.pop();
+	redSnack.unshift(redHead);
 	blueSnack.pop();
 	blueSnack.unshift(blueHead);
 	
 }
+
+//check if one has eaten other or crash onto the wall
+function checkColl(params) {
+	var blueHead = blueSnack[0];
+	var redHead = redSnack[0];
+	// check eat
+	for (var i = 0;i < redSnack.length; i++){
+		if (blueHead.x == redSnack[i].x && blueHead.y == redSnack[i].y && blueHead != redHead){
+			blueEat = true;
+		}
+	}
+	
+	for (var i = 0; i < blueSnack.length; i++){
+		if (redHead.x == blueSnack[i].x && redHead.y == blueSnack[i].y && redHead != blueHead){
+			redEat = true;
+		}
+	}
+	
+	//check blue wall
+	if (blueHead.x == 0){
+		if (blueDrt == direction.Left){
+			redEat = true;
+		}
+	}
+	if (blueHead.x == width - 1){
+		if (blueDrt == direction.Right){
+			redEat = true;
+		}
+	}
+	if (blueHead.y == 0){
+		if (blueDrt == direction.Up){
+			redEat = true;
+		}
+	}
+	if (blueHead.y == height - 1){
+		if (blueDrt == direction.Down){
+			redEat = true;
+		}
+	}
+	//check red wall
+	if (redHead.x == 0){
+		if (redDrt == direction.Left){
+			blueEat = true;
+		}
+	}
+	if (redHead.x == width - 1){
+		if (redDrt == direction.Right){
+			blueEat = true;
+		}
+	}
+	if (redHead.y == 0){
+		if (redDrt == direction.Up){
+			blueEat = true;
+		}
+	}
+	if (redHead.y == height - 1){
+		if (redDrt == direction.Down){
+			blueEat = true;
+		}
+	}
+	
+	//check if they crash in heads
+	if (blueHead == redHead){
+		reset = true;
+	}
+}
+
+//check if one has wined
+function checkWin() {
+	if (redSnack.length == 1 && blueEat){
+		blueWin = true;
+	}
+	if (blueSnack.length == 1 && redEat){
+		redWin = true;
+	}
+}
+
 
 //Key listener
 document.onkeydown = function (e) {
@@ -151,14 +276,14 @@ document.onkeydown = function (e) {
 		}
 
 	}
-}
+};
 
 // Clone function for move
 //Assigning in js is in reference
-Object.prototype.clone=function(){
-var newObj = new Object();
-for(elements in this){
-newObj[elements] = this[elements];
-}
-return newObj;
-}
+Object.prototype.clone = function () {
+	var newObj = new Object();
+	for (elements in this) {
+		newObj[elements] = this[elements];
+	}
+	return newObj;
+};
