a380c8edc44e5d572d5a842f56a27bbbb6993a56
xzk <xiaozhikang0916@gmail.com>
2015-06-12 15:11:55 +0800

    draw function

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@
 var width = 72;
 var height = 36;
 var cellSize = 8;
-var game;
+var c = document.getElementById('gameCanvas');
 // Direction
 var direction = {'Up' : 1,
 	'Down' : 2,
@@ -17,19 +17,21 @@ var redDrt = direction.Up;
 var blueDrt = direction.Down;
 
 onload = function () {
-	var c = document.getElementById('gameCanvas');
-	c.style.height = height * cellSize + 'px';
-	c.style.width = width * cellSize + 'px';
+//	c.style.height = height * cellSize + 'px';
+//	c.style.width = width * cellSize + 'px';
+	c.style.height = 800 + 'px';
+	c.style.width = 800 + 'px';
 	c.style.border = '1px solid #c3c3c3';
-	game = c.getContext('2d');
 	redSnack = new Array;
 	blueSnack = new Array;
+	resetPosition(5,5);
+	draw();
 }
 
-function refreshPosition(length1 = 0, length2 = 0) {
+function resetPosition(length1, length2) {
 	var redLength;
 	var blueLength;
-	if (length1 == 0 || length2 == 0){		
+	if (length1 == undefined || length2 == undefined){		
 	 	redLength = redSnack.length;
 		blueLength = blueSnack.length;
 	} else {
@@ -59,4 +61,29 @@ function refreshPosition(length1 = 0, length2 = 0) {
 			'y' : y*cellSize});
 			y -= 1;
 	}
+}
+
+//function to draw frame of the game
+function draw() {
+	var game = c.getContext('2d');
+	//clear the canvas before a new frame
+//	game.clearRect(0, 0, width * cellSize, height * cellSize);
+	
+	//draw red snack
+	game.fillStyle = "#ff0000";
+	//game.strokeStyle = "#ff0000";
+	for (var i = 0; i < redSnack.length; i++){
+		var point = redSnack[i];
+		console.log(point.x + ' ' + point.y);
+		game.fillRect(point.x, point.y, cellSize, cellSize);
+		game.fillRect(20,100,5,5);
+	}
+	
+	//draw blue snack
+	game.fillStyle = "#0088ff";
+	game.strokeStyle = "#0088ff";
+	for (var i = 0; i < blueSnack.length; i++){
+		game.fillRect(blueSnack[i]['x'], blueSnack[i]['y'], cellSize, cellSize);
+	}
+	
 }
